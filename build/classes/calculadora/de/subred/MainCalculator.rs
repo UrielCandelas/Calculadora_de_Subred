27d75b14e6ea0dd7d9253006202d4ef0696569c8
UrielCandelas <candelas.meza.uriel@gmail.com>
2023-09-25 23:16:56 -0600

    first commit

---
+++ build/classes/calculadora/de/subred/MainCalculator.rs
@@ -0,0 +1,8 @@
+calcuator.MainCalculator
+calcuator.MainCalculator$4
+calcuator.MainCalculator$3
+calcuator.MainCalculator$2
+calcuator.MainCalculator$1
+calcuator.MainCalculator$7
+calcuator.MainCalculator$6
+calcuator.MainCalculator$5
